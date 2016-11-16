8778d030ebddcf309e9e6d09486c38dfbcaae70d
Alexander Schuneman <alex_schuneman@outlook.com>
2016-11-16 18:43:21 +0000

    edits to second version of blog introducing changes in overall layout

---
+++ Sunshine/app/src/main/java/app/sh/surge/kaxschme/kozzimodo/sunshine/MainActivity.java
@@ -0,0 +1,13 @@
+package app.sh.surge.kaxschme.kozzimodo.sunshine;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
