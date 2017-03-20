e1da558e32172c489cd9b0a72fe6fe4c616389b2
Alexander Schuneman <alex_schuneman@outlook.com>
2017-03-20 12:39:55 +0000

    Renamed file to main.cpp Added main

---
+++ C++ Projects/SimpleCalculator/main.cpp
@@ -0,0 +1,17 @@
+/
+//  main.cpp
+//  Chapter6Exercise2
+//
+//  Created by Korrol Schuneman on 20/03/2017.
+//  Copyright © 2017 Korrol Alexander Schuneman. All rights reserved.
+//  Simple calculator to implement a set of grammar rules
+
+#include "std_lib_facilities.h"
+
+int main() {
+    cout << "Hello, and welcome to my simple calculator program!\n"
+         << "Here are the instructions for using the calculator:\n"
+         << "Enter an expression that you want to evaluate followed by a semi-colon, ';' to finish the expression\n"
+         << "To terminate the program, use 'q' for 'QUIT'\n";
+    return 0;
+}
